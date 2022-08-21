513dd623166317bb9b33acc4ff93e2c3123f22db
afzal-sohag <sohagpe1@gmail.com>
2022-08-21 17:44:51 +0600

    first commit

---
+++ components/Cards/GoogleMap/index.js
@@ -0,0 +1,17 @@
+const GoogleMap = ({ width, height, className, opacity }) => {
+  return (
+    <div className={className || " bg-gray-800"}>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3618.056562934266!2d91.97082851432155!3d24.93014294860234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3750552bc71c899d%3A0x804e438bcc32b390!2sMetropolitan%20University!5e0!3m2!1sen!2sbd!4v1661081644623!5m2!1sen!2sbd"
+        width={width || "100%"}
+        height={height || "450px"}
+        style={{ opacity: opacity || ".7" }}
+        allowFullScreen=""
+        loading="lazy"
+      ></iframe>
+      
+    </div>
+  );
+};
+
+export default GoogleMap;
